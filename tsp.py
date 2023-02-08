3108be80175604fe4d17f4862b1d9ee50ef568ca
Piotr Bosowski <piotr.bosowski@gmail.com>
2023-02-08 04:51:05 +0100

    Working TSP

--- tsp.py
+++ tsp.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt, animation
 
 """
 Zadanie E.4 (Piotr Bosowski)
@@ -22,39 +23,54 @@ def criterion(cities_positions, order):
     return intermediate_steps + borderlands
 
 
-def crossover(ind_1, ind_2):
-    unused_cities = np.ones_like(ind_1, dtype=bool)
+def crossover(ind_1, ind_2, mutation_chance=0.2):
     random_mask = np.random.randint(low=0, high=2,
-                                    size=unused_cities.shape,
+                                    size=ind_1.shape,
                                     dtype=bool)
-    output = np.empty_like(unused_cities, dtype=int)
+    used_cities = np.zeros_like(random_mask, dtype=bool)
+    output = np.empty_like(random_mask, dtype=int)
     # need to do sequentially
-    for i in range(len(unused_cities)):
-        cities_left = unused_cities * random_mask
-        if cities_left[i]:
-            output[i] = ind_2[i]
+    for i in range(len(random_mask)):
+        if random_mask[i]:
+            city_index = ind_2[i] if not used_cities[ind_2[i]] else ind_1[i]
         else:
-            output[i] = ind_1[i]
-        unused_cities[output[i]] = False
+            city_index = ind_1[i] if not used_cities[ind_1[i]] else ind_2[i]
+        if used_cities[city_index]:
+            city_index = np.random.choice(np.where(used_cities == 0)[0])
+        used_cities[city_index] = True
+        output[i] = city_index
+    assert (len(np.unique(output)) == len(ind_1))
+    if np.random.uniform() < mutation_chance:
+        swap_1, swap_2 = np.random.choice(len(output), replace=False, size=2)
+        output[[swap_1, swap_2]] = output[[swap_2, swap_1]]
     return output
 
 
-def tsp_demonstration(n_cities=10,
+def tsp_demonstration(n_cities=30,
                       ndim=2,
-                      epochs=10,
-                      n_individuals=10,
-                      n_children=5):
+                      epochs=400,
+                      n_individuals=2500,
+                      n_children=1500):
     n_parents = n_individuals - n_children
     cities_positions = np.random.uniform(size=(n_cities, ndim))
     population = np.vstack([np.random.choice(n_cities,
                                              size=n_cities,
                                              replace=False)
                             for _ in range(n_individuals)])
+    # initialization:
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.scatter([0.0] + [x for x, _ in cities_positions],
+               [0.0] + [y for _, y in cities_positions])
+    images = []
     for epoch in range(epochs):
         fitnesses = np.stack([criterion(cities_positions, ident)
                               for ident in population])
         order = np.argsort(fitnesses)
-        population[:, :] = population[order, :]  # sorting population according to ranks
+        # sorting population according to ranks
+        population[:, :] = population[order, :]
         fitnesses[:] = fitnesses[order]
         weights = 1 / fitnesses
         print(f'Best route so far ({epoch} epochs): {fitnesses[0]}')
@@ -64,8 +80,27 @@ def tsp_demonstration(n_cities=10,
                 size=2,
                 replace=False,
                 p=weights[:n_parents] / np.sum(weights[:n_parents]))
-            population[-i, :] = crossover(population[parents[0]],
-                                          population[parents[1]])
+            population[n_individuals - i - 1, :] = \
+                crossover(population[parents[0]],
+                          population[parents[1]])
+        # plot the best path so far:
+        path = np.concatenate(
+            ([[0.0, 0.0]], cities_positions[population[0]], [[0.0, 0.0]]),
+            axis=0)
+        title = plt.text(0.5, 1.01, f'Epoch {epoch}/{epochs}, '
+                                    f'loss: {fitnesses[0]:.4f}',
+                         horizontalalignment='center',
+                         verticalalignment='bottom', transform=ax.transAxes)
+        elements = [title]
+        for i in range(1, len(path)):
+            start = path[i - 1]
+            stop = path[i]
+            elements.append(ax.plot((start[0], stop[0]), (start[1], stop[1]),
+                                 marker='o')[0])
+        images.append(elements)
+    # Generate the animation image and save
+    animated_image = animation.ArtistAnimation(fig, images, interval=100)
+    animated_image.save('tsp_result.gif', writer='pillow')
 
 
 if __name__ == '__main__':
